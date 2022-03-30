701df96b18f207f3bb76cde8bd3140414c6e772d
acapai <andria.c@eronservice.fr>
2022-03-30 16:56:03 +0200

    *Titre commit: Index icon folder

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -1,5 +1,10 @@
 import {ReactComponent as Dashboard} from "./dashboard.svg"
-
+import {ReactComponent as logo} from "./logo.svg";
+import {ReactComponent as Home} from "./dashboard.svg";
+import {ReactComponent as Calendar} from "./calendar.svg";
+import {ReactComponent as Projects} from "./target.svg";
+import {ReactComponent as List} from "./list.svg";
+import {ReactComponent as PowerOff} from "./power-off.svg"
 export default {
-    Dashboard
+    Dashboard, logo, Home, Calendar, Projects, List,PowerOff
 }
