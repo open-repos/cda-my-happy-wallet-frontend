b58c67c45115ae82ac4a295c1dd23aa10258733c
acapai <andria.c@eronservice.fr>
2022-04-01 21:30:40 +0200

    *Titre commit: Modif des droits user sur le dossier où sera stocké notre app sur le server

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,6 @@ import { API_BASE_URL } from './constants'
 
 //Nous créons une api axios afin de la ré-utiliser plus tard
 //Pour ne pas à avoir à re renseigner l'url de base de notre api
-//Ici, nous restons en local -> "https://api.bookstore.andriacapai.com/v1/"
 
 const api = axios.create({
     baseURL: API_BASE_URL,
