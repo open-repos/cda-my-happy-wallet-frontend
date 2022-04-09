73c5f0cf96422e1b58aceedbe425909b0bd65ba2
acapai <andria.c@eronservice.fr>
2022-04-09 14:52:02 +0200

    *Titre commit: Calcul RaV sur la base des Charges et des Revenus (interne à l'application) - TODO : rajoute bouton sauvegarder (qui execute post rav) - Faire modal déclecnhé lorsque event quitte la page

--- src/js/pages/OperationsFixes.jsx
+++ src/js/pages/OperationsFixes.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../../css/Home.css"
 import imgCharges from "../../assets/icons/Charges.png";
 import imgRevenus from "../../assets/icons/Revenus.png";
@@ -6,9 +6,32 @@ import { Link } from "react-router-dom";
 import { faChevronCircleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CardOperationFixe from './../components/CardOperationFixe';
+import { useDispatch, useSelector } from "react-redux";
 
 const OperationsFixes = () => {
     console.log("INSIDE OPERATIONS FIXE")
+    const [RaVConfirmed, setRaVConfirmed] = useState(0);
+    const { charges, revenus, isLoading } = useSelector(
+      (state) => state.operationsFixes
+    );
+    const calculRaV = () => {
+      console.log("INSIDE calculRav",)
+      // console.log(
+      //   charges.data.reduce((accumulator, current) => accumulator + parseFloat(current.montant), 0)
+      // )
+      let rav = 0
+      if (charges.hasOwnProperty("data") && revenus.hasOwnProperty("data")){
+        if(charges.data != null && revenus.data != null){
+          const totalCharges =  charges.data.reduce((accumulator, current) => accumulator + parseFloat(current.montant), 0)
+          const totalRevenus=  revenus.data.reduce((accumulator, current) => accumulator + parseFloat(current.montant), 0)
+          rav = totalRevenus-totalCharges
+        }
+      } 
+     
+      
+      // setRaV(totalRevenus-totalCharges)
+      return(<h3>"Votre reste à vivre est de : "{rav} </h3>)
+    }
   return (
     <div className='home-container'>
     <div className="goback">  <Link to="/home"><FontAwesomeIcon icon={faChevronCircleLeft} /> Annuler et revenir à la page d'accueil </Link></div>
@@ -17,6 +40,7 @@ const OperationsFixes = () => {
     <CardOperationFixe name={"Mes charges"} src={imgCharges} typeOpFixe={"charges"}/>
     <CardOperationFixe name={"Mes revenus"} src={imgRevenus} typeOpFixe={"revenus"}/>
     </div>
+    {calculRaV()}
     </div>
   )
 }
